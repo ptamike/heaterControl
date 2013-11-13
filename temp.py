f4d11b66bdc687191ce6c2029d9d3e1ddc524a6f
ptamike <mike@photobyte.org>
2013-11-13 11:36:06 +0000

    Improved comments

--- temp.py
+++ temp.py
@@ -1,19 +1,24 @@
 #! /usr/bin/python
 
 
-#This is a test module for the RaspberryPi programmable heater project.
-#Here I set up and read the temperature from a couple of DS18B20+
-# 1-wire digital sensors
+# This is a test module for the RaspberryPi programmable thermostat
+# project. This module prepares and reads the temperature from a couple 
+# of DS18B20 1-wire digital sensors
 
 import os # we need this to load the 1-wire kernel modules
 
-# Start by loading the kernel modules for 1-wire interface and temp sensor
+# Start by loading the kernel modules for 1-wire interface and sensors
 os.system('sudo modprobe w1-gpio')
 os.system('sudo modprobe w1-therm')
 
 # Setup the 1-wire address of each sensor
-# NB: If need to find the address,type 'ls' in '/sys/bus/w1/devices'dir
-# you will need to have enabled the 1-wire modules using modprobe as above
+# NB: To find the addres, connect the sensors and run the two modprobe
+# commands from lines 11 & 12 above.
+# Navigate to '/sys/bus/w1/devices'dir and type 'ls'. You should see
+# the DS18B20 devices listed as 28-........... this is the file
+# address for each sensor. You can add as many as you like, just
+# connect them in parallel and the 1-wire module handles the calls
+
 
 sense1 = "/sys/bus/w1/devices/28-000004d428de/w1_slave"
 sense2 = "/sys/bus/w1/devices/28-000004d3f8b3/w1_slave"
@@ -32,7 +37,7 @@ def getTemp(sense1, sense2):
 	t1file.close()
 	t2file.close()
 
-	# Extract the temperatures from the 2nd line after a space
+	# Extract the temperatures from the end of the 2nd line
 	temp1 = text1.split("\n")[1].split(" ")[9]
 	temp2 = text2.split("\n")[1].split(" ")[9]
 	
@@ -40,10 +45,13 @@ def getTemp(sense1, sense2):
 	T1 = float((temp1[2:]))/1000
 	T2 = float((temp2[2:]))/1000
 	
-	return T1, T2
+	return T1, T2 # return the two temperatures
 
 
 while(1):
+	
+	# An endless test loop to fetch and print temperatures
+	# hit Ctl-C in the terminal to exit
 	
 	temp1, temp2 = getTemp(sense1, sense2)
 	
