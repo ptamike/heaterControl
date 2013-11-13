931a96923488110a71234da91b64b745886a0101
ptamike <mike@photobyte.org>
2013-11-13 00:01:57 +0000

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,16 +1,17 @@
 #! /usr/bin/python
 
 import os
-"This is a test module for the programmable heater project. This provides the code to
-""" setup and read the temperature from a couple of DS18B20+ 1-wire digital sensors"
+#This is a test module for the programmable heater project. This provides the code to
+#setup and read the temperature from a couple of DS18B20+ 1-wire digital sensors
+#
 
 
-os.system('sudo modprobe w1-gpio') #load 1-wire interface
-os.system('sudo modprobe w1-therm') # load temp module
+os.system('sudo modprobe w1-gpio') #load 1-wire kernel interface
+os.system('sudo modprobe w1-therm') # load temp kernel module
 
 
 while(1):
-	"Repeatedly read and print the results from two 1-wire sensors"
+	"Repeatedly read and print the results from the two 1-wire sensors"
 
 	tfile = open("/sys/bus/w1/devices/28-000004d428de/w1_slave")
 	t1file = open("/sys/bus/w1/devices/28-000004d3f8b3/w1_slave")
@@ -20,10 +21,10 @@ while(1):
 	tfile.close()
 	t1file.close()
 
-	temp = text.split("\n")[1].split(" ")[9]
+	temp = text.split("\n")[1].split(" ")[9] # extract the temperature from the 2nd o/p line
 	temp1 = text1.split("\n")[1].split(" ")[9]
-	T = float((temp[2:]))/1000
+	T = float((temp[2:]))/1000 # convert temp to decimal
 	T1 = float((temp1[2:]))/1000
 	
-	print T, "C :: ", T1, "C  "
+	print T, "C :: ", T1, "C  " # print the result
 	
