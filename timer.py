c378262737cec823af53e0cbb5087a013d9196a5
Mike Richards <mike@photobyte.org>
2013-12-02 12:05:50 +0000

    Return variable charge to tempRqd to align with main program.

--- timer.py
+++ timer.py
@@ -7,7 +7,7 @@ import time
 based on the settings in onTime, offTime, tempOn and tempOff this return value can then be used with temp.py
 module to determine whether the heater should be turned on."""
 
-onTime = 7 
+onTime = 8 
 offTime = 21
 tempOn = 18.5 # sets the on temperature
 tempOff = 5 # sets the off temperature
@@ -19,14 +19,14 @@ def timerCheck(onTime, offTime):
 	now = datetime.datetime.now() # get the current time
 	hour = now.hour # extract the hour
 	if onTime <= hour<= offTime: # decide temperature to use
-		temp = tempOn
+		tempRqd = tempOn
 	else:
-		temp = tempOff
+		tempRqd = tempOff
 		
-	return (temp)
+	return (tempRqd)
 
 while(1):
-	temp = timerCheck(onTime, offTime)	
-	print temp
+	tempRqd = timerCheck(onTime, offTime)	
+	print tempRqd
 	
 
